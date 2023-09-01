ce0f3e0081cd12f9c8821239c7af1f445096a9ac
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-09-02 00:46:52 +0700

    Layout Global

---
+++ resources/views/layouts/global.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - Dental</title>
+    <link rel="stylesheet" href="{{ asset('css/mobile.css') }}?ver={{config('app.version')}}" type='text/css' media='all'>
+    <link rel="stylesheet" href="{{ asset('css/desktop.css') }}?ver={{config('app.version')}}" type='text/css' media='(min-width: 1024px)'>
+</head>
+<body>
+    <header class="header">
+        
+    </header>
+    <main class="main">
+        @yield('content')
+    </main>
+    <footer class="footer">
+        
+    </footer>
+</body>
+</html>
--- resources/views/patient/login.blade.php
+++ resources/views/patient/login.blade.php
@@ -1,13 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - สำหรับผู้รักษา</title>
-    <link rel="stylesheet" href="{{ asset('css/mobile.css') }} ">
-</head>
-<body>
-    <p>ทดสอบ</p>
-</body>
-</html>
+@extends('layouts.global')
+@section('title') เข้าสู่ระบบ @endsection
