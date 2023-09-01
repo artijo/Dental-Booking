11eb0edf952e7a0dab2357ecc74ce3a1685bb729
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-09-02 01:26:17 +0700

    Pratien Login

--- resources/views/patient/login.blade.php
+++ resources/views/patient/login.blade.php
@@ -1,2 +1,21 @@
 @extends('layouts.global')
 @section('title') เข้าสู่ระบบ @endsection
+@section('content')
+    <div class="patient">
+        <div class="a-container">
+            <div class="login">
+                <h1>เข้าสู่ระบบ</h1>
+                <p>หากไม่พบข้อมูลโปรดติดต่อเข้าหน้าที่</p>
+                <form action="#" method="post">
+                    @csrf
+                    <label for="idcard">หมายเลขบัตรประชาชน</label><br>
+                    <input type="number" name="idcard" placeholder="หมายเลขบัตรประชาชน 14 หลัก"><br>
+                    <label for="phone">เบอร์โทรศัพท์</label><br>
+                    <input type="tel" name="phone" placeholder="หมายเลขโทรศัพท์">
+                    <input class="btn btn-login" type="submit" value="เข้าสู่ระบบ">
+                </form>
+            </div>
+            
+        </div>
+    </div>
+@endsection
