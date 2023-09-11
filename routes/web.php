ae9ddf37646f748705efb141106bde2410d715a1
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-09-11 16:23:55 +0700

    Support Login Route Fix

--- routes/web.php
+++ routes/web.php
@@ -39,7 +39,7 @@ Route::get('/admin/login', function () {
     return view('SupportAndDoctor.login');
 })->name("supports.login");
 
-Route::post('/admin/login', [SupportController::class,'checklogin']);
+Route::post('/admin/login', [SupportController::class,'checklogin'])->name('support.checklogin');
 Route::post('/admin/doctor/login',[DoctorController::class,'doctorchecklogin'])->name('doctor.checklogin');
 Route::middleware(['support.check'])->group(function () {
     Route::get('/admin', [AdminController::class,'index'])->name('admin.index');
