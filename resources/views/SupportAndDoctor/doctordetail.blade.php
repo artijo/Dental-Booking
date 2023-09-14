c817a2c263439ae6753f59063b9688dfe6b5337a
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-09-14 22:33:07 +0700

    Add Delete and Edit

--- resources/views/SupportAndDoctor/doctordetail.blade.php
+++ resources/views/SupportAndDoctor/doctordetail.blade.php
@@ -1,9 +1,29 @@
 @extends('layouts.global')
-@section('title') เข้าสู่ระบบสำหรับผู้ดูแล @endsection
+@section('title') ข้อมูลแพทย์ @endsection
 @section('content')
-<div class="supports">
-    <div class="a-container">
-    <h3>รหัสทันตแพทย์(ID):{{$doctor->doctor_id}}</h3><br>
+<div class="a-container">
+    <div class="space"></div>
+    <div class="head-title"><h1>ข้อมูลแพทย์</h1></div>
+    <div class="space"></div>
+<nav class="dashboard-nav">
+    <ul>
+        <li><a href="{{route('admin.index')}}">หน้าหลัก</a></li>
+        <li><a href="{{route('patientlist.showpatient')}}">รายชื่อผู้รักษา</a></li>
+        <li><a href="{{route('doctor.showdoctor')}}" class="current">รายชื่อแพทย์</a></li>
+        <li><a href="{{route('showcase.showcase')}}">ข้อมูลเคสการรักษา</a></li>
+        <li><a href="{{route('showcase.showbooking')}}">ข้อมูลการนัด</a></li>
+    </ul>
+</nav>
+<div class="content-dashboard">
+    <div class="flex justify-end gap-5 mb-5">
+        <a href=""><button class="btn btn-edit">แก้ไขข้อมูล</button></a>
+        <a href=""><button class="btn btn-delete">ลบข้อมูล</button></a>
+    </div>
+    <div class="content">
+        <div class="head">
+            <h3>รหัสทันตแพทย์(ID):{{$doctor->doctor_id}}</h3>
+        </div>
+    <div class="body">
     ชื่อ-นามสกุล: ทพ.{{$doctor->name_th}} {{$doctor->lastname_th}}<br>
     Fullname: DR.{{$doctor->name_en}} {{$doctor->lastname_en}}<br>
     <h3>ข้อมูลการติดต่อ(Contact)</h3>
@@ -15,6 +35,8 @@
         <li>{{$special->name_th}}</li>
         @endforeach
     </ul>
+</div>
+</div>
     </div>
     </div>
 @endsection
