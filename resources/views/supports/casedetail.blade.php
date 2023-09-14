1f438fa4d972bedb4767413870636ed22ace4768
Yosubman <piyanat.r@kkumail.com>
2023-09-15 00:55:12 +0700

    css_casedetail

--- resources/views/supports/casedetail.blade.php
+++ resources/views/supports/casedetail.blade.php
@@ -1,12 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    {{$case->case_title}}
-</body>
-</html>
+@extends('layouts.global')
+@section('title') รายละเอียดการรักษา @endsection
+@section('content')
+<div class="a-container">
+        <div class="space"></div>
+        <div class="head-title"><h1>รายละเอียดการรักษา</h1></div>
+        <div class="space"></div>
+        <nav class="dashboard-nav">
+    <ul>
+        <li><a href="{{route('admin.index')}}">หน้าหลัก</a></li>
+        <li><a href="{{route('patientlist.showpatient')}}">รายชื่อผู้รักษา</a></li>
+        <li><a href="{{route('doctor.showdoctor')}}" >รายชื่อแพทย์</a></li>
+        <li><a href="{{route('showcase.showcase')}}" class="current">ข้อมูลเคสการรักษา</a></li>
+        <li><a href="{{route('showcase.showbooking')}}">ข้อมูลการนัด</a></li>
+    </ul>
+</nav>
+<div class="content-dashboard">
+<div class="flex justify-end gap-5 mb-5">
+        <a href=""><button class="btn btn-edit">แก้ไขข้อมูล</button></a>
+    </div>
+        <div class="content">
+        <div class="head">
+            <h3>รหัสการรักษา:{{$case->caseid}}</h3>
+        </div>
+        <div class="body">
+            <h3>ประเภทการรักษา</h3>
+                <p>{{$case->case_title}}</p>
+            <h3>รายละเอียดการรักษา</h3>
+                 <ul><li>{{$case->case_detail}}</li></ul> 
+            <h3>แพทย์ที่รักษา</h3>{{$case->doctor->name_en}} 
+           <h3>วันที่นัดหมาย</h3> 
+                @foreach($case->bookings as $time)
+                        @if($time->booking_date != NULL) 
+                                {{$time->booking_date}} น.
+                         @else 
+                          ยังไม่ลงเวลานัด
+                        @endif
+                @endforeach  <br>
+            สถานะการรักษา <br>
+             @if($case->case_status === 1)รอเข้าพบ 
+                @elseif($case->case_status === 2)ไม่มาพบตามนัด 
+                @elseif($case->case_status === 3)เสร็จสิ้น
+                @endif
+                </div>
+        </div>
+    </div>
+          
+           
+</div>
+    
+
+
+@endsection
