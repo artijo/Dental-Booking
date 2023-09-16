18842c484db3e8f0e80fd79503d99f05cce4c6f6
ARTIJO <81065469+artijo@users.noreply.github.com>
2023-09-16 18:11:23 +0700

    swl

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -10,7 +10,8 @@
     <link rel="preload" href="{{ asset('fonts/LineSeed/LINESeedSansTH_W_Th.woff2') }}" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="{{ asset('fonts/Thongterm/tt-bold.woff2') }}" as="font" type="font/woff2" crossorigin>
     <link rel="preload" href="{{ asset('fonts/Thongterm/tt-reg.woff2') }}" as="font" type="font/woff2" crossorigin>
-    <link href="{{asset('js/select2/css/select2.min.css')}}" rel="stylesheet" />
+    <link href="{{asset('js/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css" media="all"/>
+    <link rel="stylesheet" href="{{ asset('js/select2/css/select2.min.css') }}" type='text/css' media='all'>
     <link rel="stylesheet" href="{{ asset('css/tailwind.css') }}?ver={{config('app.version')}}" type='text/css' media='all'>
     <link rel="stylesheet" href="{{ asset('css/mobile.css') }}?ver={{config('app.version')}}" type='text/css' media='all'>
     <link rel="stylesheet" href="{{ asset('css/desktop.css') }}?ver={{config('app.version')}}" type='text/css' media='(min-width: 990px)'>
@@ -28,6 +29,7 @@
     </footer>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script src="{{asset('js/select2/js/select2.min.js')}}"></script>
+    <script src="{{asset('js/sweetalert2/sweetalert2.min.js')}}"></script>
     <script src="{{asset('js/select2.js')}}"></script>
 </body>
 </html>
