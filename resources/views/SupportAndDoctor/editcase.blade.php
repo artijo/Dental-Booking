7eff906ab6941862b4bf846d655032be56eb9016
CocoaTony <113881404+CocoaTony@users.noreply.github.com>
2023-09-18 20:19:55 +0700

    Fix bug on edit case

--- resources/views/SupportAndDoctor/editcase.blade.php
+++ resources/views/SupportAndDoctor/editcase.blade.php
@@ -46,13 +46,16 @@
         @method('PUT')
         <div class="add-data-item">
         <label for="idcard">รหัสบัตรประชาชน(ผู้เข้ารับการรักษา)</label><br>
+        @if($case->patient)
         <input type="number" name="idcard" value="{{$case->idcard}}" disabled><br>
+        @else
+        <input type="number" name="idcard" value="ไม่พบข้อมูลผู้เข้ารักษา" placeholder='ไม่พบข้อมูลผู้เข้ารักษา' disabled><br>
+        @endif
         <label for="case_title">หัวเรื่องการรักษา</label><br>
         <input type="text" name="case_title" value="{{$case->case_title}}" max="255" required><br>
         <label for="casetype_id">รูปแบบการรักษา</label><br>
         @if(!empty($case_type) && count($case_type) > 0)
             <select name="casetype_id" id="casetype" class="casetype">
-                <option value="0" disabled selected>เลือกประเภทการรักษา</option>
                 @foreach($case_type as $list)
                 <option value="{{ $list->casetype_id }}">{{ $list->casetype_name }}</option>
                 @endforeach
@@ -63,9 +66,13 @@
         <label for='case_detail'>รายละเอียดการรักษา</label><br>
         <input type="text" name="case_detail" value="{{$case->case_detail}}"><br>
         <label for="doctor_id">รหัสนายแพทย์</label><br>
-        @if($doctor)
+        @if($case)
             <select name="doctor_id" single class="doctor">
+                @if($case->doctor)
+                <option value="{{$case->doctor_id}}" disabled='true' selected>{{$case->doctor->name_th}} {{$case->doctor->lastname_th}}</option>
+                @else
                 <option value="0" disabled='true' selected>เลือกหมอ</option>
+                @endif
             </select><br>
         @else
         <input type="text" name="doctor_id" value="ไม่มีข้อมูลทันตแพทย์" disabled><br>
